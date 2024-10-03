c4f07b42fe8f55d781216895cffa2114b835fd54
zxsxyyn <renyeyao95@gmail.com>
2024-10-03 20:17:20 +0800

    第一次提交：功能基本完善，等待测试，<<待改善；

---
+++ ByteArray.cpp
@@ -0,0 +1,312 @@
+#include "ByteArray.h"
+#include <cstring>
+#include <iomanip>
+
+void ustd::ByteArray::resize(unsigned short newsize)
+{
+    if (newsize > thecapacity)
+    {
+        reserve(newsize * 2);
+    }
+    thesize = newsize;
+}
+
+void ustd::ByteArray::advance(unsigned short value)
+{
+    readIndex+=value;
+    if(readIndex>=thesize/2){
+        compact();
+    }
+}
+
+ustd::ByteArray::ByteArray()
+{
+    array = new char[DEFALTCAPACITY];
+}
+
+ustd::ByteArray::ByteArray(unsigned short capacity) : thecapacity(capacity)
+{
+    array = new char[thecapacity];
+}
+
+ustd::ByteArray::ByteArray(const ByteArray &other) : thecapacity(other.thecapacity), thesize(other.thesize),
+readIndex(other.readIndex)
+{
+    array = new char[thecapacity];
+    memcpy(array, other.array, thesize);
+}
+
+ustd::ByteArray::ByteArray(ByteArray &&other) : array(other.array),thecapacity(other.thecapacity), thesize(other.thesize),
+readIndex(other.readIndex)
+{
+    other.array = nullptr;
+    other.thecapacity=0;
+    other.thesize=0;
+    other.readIndex=0;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator=(const ByteArray &other)
+{
+    ByteArray temp(other);
+    std::swap(*this,temp);
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator=(ByteArray &&other)
+{
+    std::swap(array,other.array);
+    std::swap(thecapacity,other.thecapacity);
+    std::swap(thesize,other.thesize);
+    std::swap(readIndex,other.readIndex);
+    return *this;
+}
+
+ustd::ByteArray::~ByteArray()
+{
+    delete[] array;
+    array = nullptr;
+}
+
+void ustd::ByteArray::reserve(unsigned short capacity)
+{
+    if (capacity <= thecapacity)
+    {
+        return;
+    }
+    thecapacity = capacity;
+    char *old = array;
+    array = new char[thecapacity];
+    memcpy(array, old, thesize);
+    delete old;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(const std::string &str)
+{
+    unsigned pos = thesize;
+    resize(thesize + str.length());
+    memcpy(array + pos, str.c_str(), str.length());
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(const char *str)
+{
+    unsigned strsize = 0;
+    const char *ptr = str;
+    while (*ptr++)
+    {
+        strsize++;
+    }
+    unsigned pos = thesize;
+    resize(thesize + strsize);
+    memcpy(array + pos, str, strsize);
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(short s_integer)
+{
+    unsigned pos = thesize;
+    resize(thesize + sizeof(short));
+    memcpy(array + pos, &s_integer, sizeof(short));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(unsigned short s_integer)
+{
+    unsigned pos = thesize;
+    resize(thesize + sizeof(unsigned short));
+    memcpy(array + pos, &s_integer, sizeof(unsigned short));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(int integer)
+{
+    unsigned pos = thesize;
+    resize(thesize + sizeof(int));
+    memcpy(array + pos, &integer, sizeof(int));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(long l_integer)
+{
+    unsigned pos = thesize;
+    resize(thesize + sizeof(long));
+    memcpy(array + pos, &l_integer, sizeof(long));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(unsigned uinteger)
+{
+    unsigned pos = thesize;
+    resize(thesize + sizeof(unsigned));
+    memcpy(array + pos, &uinteger, sizeof(unsigned));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator<<(unsigned long ul_integer)
+{
+    unsigned pos = thesize;
+    resize(thesize + sizeof(unsigned long));
+    memcpy(array + pos, &ul_integer, sizeof(unsigned long));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::append(const char *buf, unsigned short size)
+{
+    unsigned pos = thesize;
+    resize(thesize + size);
+    memcpy(array, buf, size);
+    return *this;
+}
+
+void ustd::ByteArray::compact()
+{
+    unsigned short surpsize=size();
+    memmove(array,array+readIndex,surpsize);
+    readIndex=0;
+    resize(surpsize);
+}
+
+short ustd::ByteArray::readShort()
+{
+    if(size()<sizeof(short)){
+        throw("剩余空间不足以读取short");
+        return 0;
+    }
+    short value;
+    memcpy(&value,array+readIndex,sizeof(short));
+    advance(sizeof(short));
+    return value;
+}
+
+int ustd::ByteArray::readInt()
+{
+    if(size()<sizeof(int)){
+        throw("剩余空间不足以读取int");
+        return 0;
+    }
+    int value;
+    memcpy(&value,array+readIndex,sizeof(int));
+    advance(sizeof(int));
+    return value;
+}
+
+long ustd::ByteArray::readLong()
+{
+    if(size()<sizeof(long)){
+        throw("剩余空间不足以读取long");
+        return 0;
+    }
+    long value;
+    memcpy(&value,array+readIndex,sizeof(long));
+    advance(sizeof(long));
+    return value;
+}
+
+bool ustd::ByteArray::read(void *buf, unsigned short size_)
+{
+    if(size()<size_){
+        return false;
+    }
+    memcpy(buf,array+readIndex,size_);
+    advance(size_);
+    return true;
+}
+
+std::string ustd::ByteArray::read(unsigned short size_)
+{
+    if(size()<size_)
+        return std::string();
+    std::string str(array,size_);
+    advance(size_);
+    return str;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator>>(short &value)
+{
+    if(size()<sizeof(short)){
+        throw("剩余空间不足以读取short");
+        return *this;
+    }
+    memcpy(&value,array+readIndex,sizeof(short));
+    advance(sizeof(short));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator>>(int &value)
+{
+    if(size()<sizeof(int)){
+        throw("剩余空间不足以读取short");
+        return *this;
+    }
+    memcpy(&value,array+readIndex,sizeof(int));
+    advance(sizeof(int));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator>>(long &value)
+{
+    if(size()<sizeof(long)){
+        throw("剩余空间不足以读取short");
+        return *this;
+    }
+    memcpy(&value,array+readIndex,sizeof(long));
+    advance(sizeof(long));
+    return *this;
+}
+
+ustd::ByteArray &ustd::ByteArray::operator>>(char &c)
+{
+    if(size()<sizeof(char)){
+        throw("剩余空间不足以读取short");
+        return *this;
+    }
+    memcpy(&c,array+readIndex,sizeof(char));
+    advance(sizeof(char));
+    return *this;
+}
+
+unsigned short ustd::ByteArray::size() const
+{
+    return thesize-readIndex;
+}
+
+unsigned short ustd::ByteArray::capacity() const
+{
+    return thecapacity;
+}
+
+void ustd::ByteArray::clear()
+{
+    thesize=0;
+    readIndex=0;
+}
+
+bool ustd::ByteArray::empty() const
+{
+    return size()==0;
+}
+
+const char *ustd::ByteArray::c_str() const
+{
+    return array;
+}
+
+namespace ustd
+{
+    std::ostream& operator<<(std::ostream &os, const ustd::ByteArray &byteArray)
+    {
+        os << '[';
+        for (unsigned short i = byteArray.readIndex; i < byteArray.thesize-1; ++i)
+        {
+            // 设置为16进制输出，每个字节占两位，前导补0
+            os << std::hex << std::setw(2) << std::setfill('0')
+               << static_cast<int>(static_cast<unsigned char>(byteArray.array[i])) << " ";
+        }
+        os << std::hex << std::setw(2) << std::setfill('0')
+               << static_cast<int>(static_cast<unsigned char>(byteArray.array[byteArray.thesize]));
+        os << ']';
+        // 恢复流的格式
+        os << std::dec; // 回到十进制模式
+        return os;
+    }
+}
---
+++ ByteArray.h
@@ -0,0 +1,63 @@
+#ifndef BYTEARRAY_H
+#define BYTEARRAY_H
+#include <string>
+#include <iostream>
+
+namespace ustd
+{
+    class ByteArray
+    {
+    private:
+        friend class TcpSocket;
+        inline static const int DEFALTCAPACITY=15; //默认容量
+        char* array;//指向数据的指针
+        unsigned short thecapacity=DEFALTCAPACITY;//容量
+        unsigned short thesize=0;//已经存放的字节数
+        unsigned short readIndex=0;//读取索引
+        void resize(unsigned short);
+        void advance(unsigned short value);
+    public:
+        inline static const unsigned short MAX_CAPACITY=65535;
+        ByteArray();
+        ByteArray(unsigned short capacity_);
+        ByteArray(const ByteArray& other);
+        ByteArray(ByteArray&& other);
+        ByteArray& operator=(const ByteArray& other);
+        ByteArray& operator=(ByteArray&& other);
+        ~ByteArray();
+
+        void reserve(unsigned short capacity);
+
+        ByteArray& operator<<(const std::string& str);
+        ByteArray& operator<<(const char* str);
+        ByteArray& operator<<(short s_integer);
+        ByteArray& operator<<(unsigned short s_integer);
+        ByteArray& operator<<(int integer);
+        ByteArray& operator<<(long l_integer);
+        ByteArray& operator<<(unsigned uinteger);
+        ByteArray& operator<<(unsigned long ul_integer);
+        ByteArray& append(const char* buf,unsigned short size);
+
+        void compact();//压缩空间
+        short readShort();
+        int readInt();
+        long readLong();
+        bool read(void* buf,unsigned short size);
+        std::string read(unsigned short size);
+
+        ByteArray& operator>>(short& value);
+        ByteArray& operator>>(int& value);
+        ByteArray& operator>>(long& value);
+        ByteArray& operator>>(char& c);
+        
+        unsigned short size()const;
+        unsigned short capacity()const;
+        void clear();
+        bool empty()const;
+        const char* c_str()const;
+
+        friend std::ostream& operator<<(std::ostream& os,const ByteArray& array);
+    };
+}
+
+#endif
